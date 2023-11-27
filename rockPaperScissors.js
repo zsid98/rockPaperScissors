adb5ebe40c7a350a640aba3d26e8ccd8615ad140
zsid98 <138807049+zsid98@users.noreply.github.com>
2023-11-27 09:42:07 +0100

    added interactive js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,62 +1,89 @@
-// Get a random integer between two values; minimum inclusive, maximum exclusive
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
-// Get the computer choice
-function getComputerChoice() {
-    x = getRandomInt(0, 3);
-    if (x === 0) {
-        return "rock";
-    } else if (x === 1) {
-        return "paper";
-    } else if (x === 2) {
-        return "scissors";
+// Simulate one round
+let p = 0, c = 0;
+let unknown = document.querySelector("#unknown");
+
+function playRound(playerSelection) {
+
+    // Get a random integer between two values; minimum inclusive, maximum exclusive
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
     }
-}
-// Simulate a 5 round game
-function game() {
-    p = 0;
-    c = 0;
-    // Simulate one round
-    function playRound() {
-        playerSelection = prompt("Rock, paper, or scissors? ").toLowerCase();
-        if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-            return "Invalid input";
-        }
-        computerSelection = getComputerChoice();
-        if (playerSelection === computerSelection) {
-            console.log("Tie!");
-        } else if (playerSelection === "rock" && computerSelection === "paper") {
-            console.log("You lose! Paper beats rock");
-            c++;
-        } else if (playerSelection === "rock" && computerSelection === "scissors") {
-            console.log("You win! Rock beats scissors");
-            p++;
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
-            console.log("You win! Paper beats rock");
-            p++;
-        } else if (playerSelection === "paper" && computerSelection === "scissors") {
-            console.log("You lose! Scissors beat paper");
-            c++;
-        } else if (playerSelection === "scissors" && computerSelection === "rock") {
-            console.log("You lose! Rock beats scissors");
-            c++;
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            console.log("You win! Scissors beat paper");
-            p++;
+    // Get the computer choice
+    function getComputerChoice() {
+        x = getRandomInt(0, 3);
+        unknown.style.border = "3px solid white";
+        setTimeout(() => {
+            unknown.style.border = "none";
+        }, "400");
+        if (x === 0) {
+            unknown.src = "./img/rockComp.jpg";
+            return "rock";
+        } else if (x === 1) {
+            unknown.src = "./img/paperComp.jpg";
+            return "paper";
+        } else if (x === 2) {
+            unknown.src = "./img/scissorsComp.jpg";
+            return "scissors";
         }
     }
-    for (let i = 0; i < 5; i++) {
-        playRound();
+    computerSelection = getComputerChoice();
+
+    let playerWins = document.querySelector("#player-wins");
+    let compWins = document.querySelector("#computer-wins");
+
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        c++;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        p++;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        p++;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        c++;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        c++;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        p++
     }
-    if (p === c) {
-        alert("It's a tie!");
-    } else if (p > c) {
-        alert("You win the game!");
-    } else if (p < c) {
-        alert("You lose the game!")
-    }
-    return;
+    playerWins.textContent = p;
+    compWins.textContent = c;
 }
+
+// Play round by clicking a choice button
+let buttons = document.querySelector("#player-img");
+let endResult = document.querySelector("#endResult");
+buttons.addEventListener('click', event => {
+    let choice = event.target;
+
+    playRound(choice.id);
+    if (p === 5 || c === 5) {
+        // After a game is over, display new game button and remove all h2 headings and images 
+        document.querySelectorAll("h2").forEach(e => e.remove());
+        document.querySelectorAll("img").forEach(e => e.remove());
+    
+        endResult.style.fontSize = "40px";
+        endResult.style.textAlign = "center";
+        let endDiv = document.querySelector("#endDiv");
+        let newGame = document.createElement("button");
+        newGame.id = "newGame";
+        newGame.textContent = "New Game";
+        endDiv.appendChild(newGame);
+        newGame.addEventListener("click", () => {
+            window.location.reload();
+        })
+    }
+
+    if (p === 5) {
+        endResult.textContent = "You won!";
+        p = 0;
+        c = 0;
+    } else if (c === 5) {
+        endResult.textContent = "You lost!"
+        p = 0;
+        c = 0;
+    }
+}); 
+
+
+
